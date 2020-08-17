d978e0176adaa535376390b1ffa6a7f764a452ae
mariya.atamas <varteya@gmail.com>
2020-08-17 21:53:48 +0300

    Homework 3 version 1

--- readme.txt
+++ readme.txt
@@ -1,6 +1,8 @@
 For run tests:
 1. Make sure, that you have tuned environment (adb, local variables, etc);
-2. Run emulator;
-3. Run Appium server;
+2. Log in EPAM Mobile Cloud Service;
+3. Write your personal token in pom.xml;
+4. Choose device ant write deviceName or bundleID in *.xml file for the chosen profile;
+4.a If you want to test native application, install it on the device via mobile cloud tools;
 4. Change properties in nativeTNG.xml and webTNG.xml, if you need it;
-5. Run tests by maven profiles native or web.
+5. Run tests by maven profiles.
--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -3,38 +3,49 @@ package pageObjects;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject  {
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
     WebElement registerBtn;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
     WebElement registrationEmailField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
     WebElement registrationUsernameField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
     WebElement registrationPasswordField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
     WebElement registrationConfirmPasswordField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
     WebElement registerNewAccountBtn;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
     WebElement loginUsernameField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
     WebElement loginPasswordField;
 
+    @iOSXCUITFindBy(xpath = "PLACE_FOR_XPATH")
     @AndroidFindBy(xpath = "\t/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.TextView")
     WebElement actionBar;
 
